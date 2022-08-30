bcc1a90f9babb98d0ddf3b51af71f0267f6637dd
liaochungyi <lyle.liao@pinnov.com>
2022-08-31 00:09:25 +0800

    complete resistor-color

---
+++ resistor-color/resistor-color.ts
@@ -0,0 +1,28 @@
+export const colorCode = (color: string): number => {
+  const colors = [
+    'black',
+    'brown',
+    'red',
+    'orange',
+    'yellow',
+    'green',
+    'blue',
+    'violet',
+    'grey',
+    'white',
+  ];
+  return colors.indexOf(color);
+}
+
+export const COLORS = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
