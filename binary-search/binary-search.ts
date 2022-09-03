b623b91f4d5d7558053999eda8511a8552929a99
liaochungyi <lyle.liao@pinnov.com>
2022-09-03 16:16:15 +0800

    add recursive solution in binary-search

--- binary-search/binary-search.ts
+++ binary-search/binary-search.ts
@@ -3,18 +3,32 @@ export function find(haystack: number[], needle: number): number {
   // if (result === -1 ) throw new Error('Value not in array');
   // return result;
 
-  let start = 0;
-  let end = haystack.length - 1;
-  while (start <= end) {
-    const mid = Math.floor((start + end) / 2);
-    const target = haystack[mid];
-    if (target > needle) {
-      end = mid - 1;
-    } else if (target < needle) {
-      start = mid + 1;
-    } else {
-      return mid;
-    }
-  }
-  throw new Error('Value not in array'); 
+
+  // looping
+  // let start = 0;
+  // let end = haystack.length - 1;
+  // while (start <= end) {
+  //   const mid = Math.floor((start + end) / 2);
+  //   const target = haystack[mid];
+  //   if (target > needle) {
+  //     end = mid - 1;
+  //   } else if (target < needle) {
+  //     start = mid + 1;
+  //   } else {
+  //     return mid;
+  //   }
+  // }
+  // throw new Error('Value not in array'); 
+
+
+  // recursive
+  if (!haystack.length) throw new Error('Value not in array');
+  
+  const mid = Math.floor(haystack.length / 2);
+  if (haystack[mid] > needle) {
+    return find(haystack.slice(0, mid), needle);
+  } else if (haystack[mid] < needle) {
+    return mid + 1 + find(haystack.slice(mid + 1), needle);
+  } 
+  return mid;
 }
